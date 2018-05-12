1226d7cfa5d71b727045ef9ebce66b7185fbf3b9
Anya <hannakilpatrick@gmail.com>
2018-05-12 15:10:54 -0400

    syncing sequelize model

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 var PORT = process.env.PORT||3000;
 
 var app = express();
-
+var db = require("./models");
 // serve static content from the "public" directory
 app.use(express.static("public"));
 
@@ -23,6 +23,8 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-app.listen(PORT, function(){
-    console.log("Listening at localhost: " + PORT);
-})
+db.sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+});
