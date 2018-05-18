2e10346a132583bb8903b31cf64132f1d72390a8
Anya <hannakilpatrick@gmail.com>
2018-05-17 22:11:37 -0400

    made a change in route

--- routes/routes.js
+++ routes/routes.js
@@ -40,7 +40,7 @@ module.exports = function(app){
     })
 
     app.post("/signup", passport.authenticate("local-signup", {
-        successRedirect : "/signup2", // redirect to the secure profile section
+        successRedirect : "/profile", // redirect to the secure profile section
         failureRedirect : "/signup", // redirect back to the signup page if there is an error
         // failureFlash : true // allow flash messages
     }));
