19c68dc8b80b3abca659acfb7edc17ed72d4cbb5
Anya <hannakilpatrick@gmail.com>
2018-05-22 16:16:21 -0400

    added signup validation

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,10 +1,3 @@
-// materialize dropdown for choosing flag of native language
-// $('.dropdown-trigger').on("click", function(){
-//   console.log("I am clicking");
-//   $(this).dropdown();
-// });
-// console.log("sanity")
-
 // materialize drop down
 $(document).ready(function () {
   $('.dropdown-button').dropdown({
@@ -17,10 +10,39 @@ $(document).ready(function () {
     belowOrigin: false // Displays dropdown below the button
   });
 });
-
+// enable select input
 $(document).ready(function() {
   $('select').material_select();
 });
-// $(document).ready(function(){
-//   $('select').formSelect();
-// });
+
+// validate sign-up form
+$("#signUpForm").on('submit', function (e) {
+  debugger
+  // var focusSet = false;
+    var usernameL = $("#newUserName").val().trim().length;
+    if (usernameL < 5 ||usernameL > 15){
+      $("#newUserName").parent().next(".validation").remove() //remove before inserting, to prevent from creating many divs on top of each other
+      $("#newUserName").parent().after("<div class='validation' style='color:black;margin-bottom: 20px;'>Please enter username (5-15 characters)</div>");
+      e.preventDefault();
+    }else {
+      $("#newUserName").parent().next(".validation").remove(); // remove it
+    }
+
+    var passwordL = $("#newUserPassword").val().trim().length;
+    if(passwordL <5 || passwordL >10){
+      $("#newUserPassword").parent().next(".validation").remove()
+      $("#newUserPassword").parent().after("<div class='validation' style='color:black;margin-bottom: 20px;'>Please enter password (5-10 characters)</div>");
+      e.preventDefault();
+    }else {
+      $("#newUserPassword").parent().next(".validation").remove(); // remove it
+    }
+
+    var aboutL = $("#newUserAbout").val().trim().length;
+    if(aboutL <10 || aboutL >200){
+      $("#newUserAbout").parent().next(".validation").remove();
+      $("#newUserAbout").parent().after("<div class='validation' style='color:black;margin-bottom: 20px;'>Write about yourself (10-200 characters)</div>");
+      e.preventDefault();
+    }else {
+      $("#newUserAbout").parent().next(".validation").remove(); // remove it
+    }
+});  
